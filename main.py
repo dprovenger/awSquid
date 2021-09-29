62e011690201801f30a72dd5e9787c530b26d109
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-09-28 22:31:00 -0500

    Changing some formatting

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import boto3
 
 # List of aws profiles
-aws_prof=['default', 'jarrieta-awscli']
-# Multiple profiles:
-# aws_prof=['default', 'default']
+aws_prof=['jarrieta-awscli']
 
+# Multiple profiles:
+# aws_prof=['default', 'default', 'profileid']
 
 # List of Regions and EC2 instances with in each region
 for each_prof in aws_prof:
     print('')
-    print('== ',each_prof,' profile and EC2 instances == ')
+    print('== Profile -',each_prof,'-- EC2 instances == ')
     session=boto3.Session(profile_name=each_prof,region_name='us-east-2')
     client=session.client('ec2')
     all_regions=client.describe_regions()
@@ -19,14 +19,14 @@ for each_prof in aws_prof:
     for each_reg in list_of_regions:
         session=boto3.Session(region_name=each_reg)
         resource=session.resource('ec2')
-        print(each_reg," -- EC2s:")
+        print("Region:",each_reg)
         for each_in in resource.instances.all():
-            print("-->  ",each_reg," -- ",each_in.id,each_in.state['Name'])
+            print("      -",each_reg,"-- Instance ID:",each_in.id,"-- Instance State:",each_in.state['Name'])
 
 # List of Regions and Classic ELBs with in each region
 for each_prof in aws_prof:
     print('')
-    print('== ',each_prof,' profile and Classic Load Balancers == ')
+    print('== Profile -',each_prof,'-- Classic ELBs == ')
     session=boto3.Session(profile_name=each_prof,region_name='us-east-2')
     client=session.client('ec2')
     all_regions=client.describe_regions()
@@ -36,17 +36,17 @@ for each_prof in aws_prof:
     for each_reg in list_of_regions:
         session=boto3.Session(region_name=each_reg)
         resource=session.client('elb')
-        print(each_reg," -- Classic ELBs:")
+        print("Region -",each_reg)
         for each in resource.describe_load_balancers()['LoadBalancerDescriptions']:
             #for each_in in each['LoadBalancerName']:
             #print(each)
-            print("-->  ",each_reg," -- ",each['LoadBalancerName'])
+            print("       -",each_reg,"-- Classic ELB Name:",each['LoadBalancerName'])
                 #print('each_in.LoadBalancerName', 'each_in.DNSName')
 
 # List of Regions and App/Net ELBs with in each region
 for each_prof in aws_prof:
     print('')
-    print('== ',each_prof,' profile and App/Net Load Balancers == ')
+    print('== Profile -',each_prof,'-- App/Net ELBs == ')
     session=boto3.Session(profile_name=each_prof,region_name='us-east-2')
     client=session.client('ec2')
     all_regions=client.describe_regions()
@@ -56,6 +56,6 @@ for each_prof in aws_prof:
     for each_reg in list_of_regions:
         session=boto3.Session(region_name=each_reg)
         resource=session.client('elbv2')
-        print(each_reg," -- APP/NET ELBs:")
+        print("Region -",each_reg)
         for each in resource.describe_load_balancers()['LoadBalancers']:
-            print("-->  ",each_reg," -- ",each['LoadBalancerName']," -- ",each['Type'])
+            print("       -",each_reg,"-- ELB Name:",each['LoadBalancerName'],"-- ELB Type:",each['Type'])
