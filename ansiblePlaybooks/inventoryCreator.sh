c8ddad40994f67fa7da95a88c4206e38a02ea3f1
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-10-29 00:31:43 -0500

    Deleting extra comment

--- ansiblePlaybooks/inventoryCreator.sh
+++ ansiblePlaybooks/inventoryCreator.sh
@@ -7,6 +7,5 @@ if [[ $1 = "" ]]; then
   echo -e "Tenant's inventory file must exist in output dir \n"
 else
   echo '[linux]' > ansibleInventoryFiles/$1"-hosts"
-  #for h in `cat output/$file` do; awk -F "," '{print $3 " " $1}' >> ansibleInventory/$1"-hosts"; done
   for h in `cat ../output/$file | grep -v Name`; do echo $h | awk -F "," '{print $3 " " $1}' >> ansibleInventoryFiles/$1"-hosts"; done
 fi 
