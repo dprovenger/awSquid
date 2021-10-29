3b9e586a3a42b933668e772d5af482ecaa94d0ad
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-10-29 00:09:57 -0500

    Adding script usage

--- ansiblePlaybooks/inventoryCreator.sh
+++ ansiblePlaybooks/inventoryCreator.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+file=$1"-ec2_invent.csv"
+
+if [[ $1 = "" ]]; then
+  echo -e "\nSample Usage I: $0 C3"
+  echo -e "Sample Usage II: $0 C101"
+  echo -e "Tenant's inventory file must exist in output dir \n"
+else
+  echo '[linux]' > ansibleInventoryFiles/$1"-hosts"
+  #for h in `cat output/$file` do; awk -F "," '{print $3 " " $1}' >> ansibleInventory/$1"-hosts"; done
+  for h in `cat ../output/$file | grep -v Name`; do echo $h | awk -F "," '{print $3 " " $1}' >> ansibleInventoryFiles/$1"-hosts"; done
+fi 
