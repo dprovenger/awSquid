e361a6e627ee48c96b0a80dddb65317a2fd97624
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-11-27 13:59:25 -0600

    Adding AWS EC2 option

--- awsquid.py
+++ awsquid.py
@@ -1,5 +1,9 @@
+import boto3
+import csv
 import sys
 from typing import Dict, List
+
+tenants = ['TENANT1', 'TENANT2']
 
 def main() -> None:
     """Entrypoint of program."""
@@ -12,27 +16,61 @@ def aws_options():
         print("\n Tenant ID missing, pls reference usage:")
         print(usage_help_arg())
         exit()
-    if sys.argv[2] in ["TENANT1","TENANT2"]:
+    if sys.argv[2] in tenants:
         if len(sys.argv) == 3:
             print("\n Task missing, pls reference usage:")
             print(usage_help_arg())
             exit()
         if sys.argv[3] == "EC2":
             print(aws_ec2())
+            exit()
         if sys.argv[3] == "ELB":
             print(aws_elb())
+            exit()
         elif ((sys.argv[3] != "EC2") and (sys.argv[3] != "ELB")):
             print("\n Invalid task, pls reference usage:")
             print(usage_help_arg())
             exit()
 
-    elif sys.argv[2] not in ["TENANT1","TENANT2"]:
+    elif sys.argv[2] not in tenants:
         print("\n Invalid Tenant ID, pls reference usage:")
         print(usage_help_arg())
         exit()
 
 def aws_ec2():
-    print("Welcome to AWS EC2 Section")
+    each_prof = sys.argv[2]
+    print('')
+    print('== Profile -',each_prof,'-- EC2 instances == ')
+    session=boto3.Session(profile_name=each_prof,region_name='us-east-2')
+    client=session.client('ec2')
+    all_regions=client.describe_regions()
+    list_of_regions=[]
+    for each_reg in all_regions['Regions']:
+        list_of_regions.append(each_reg['RegionName'])
+    ins_file=open('output/' + each_prof + '-ec2_invent.csv','w',newline='')
+    ins_data=csv.writer(ins_file)
+    ins_data.writerow(['Name','Instance','IP','MacAddress','Environment','Platform','OS','RunAfterHours','InstanceType'])
+    for each_reg in list_of_regions:
+        session=boto3.Session(profile_name=each_prof,region_name=each_reg)
+        resource=session.resource('ec2')
+        print("Auditing region",each_reg)
+        for each_in in resource.instances.all():
+            mac = ""
+            for iface in each_in.network_interfaces:
+                mac = iface.mac_address
+            ins_name = ""
+            run_af_hrs = ""
+            env = ""
+            for tags in each_in.tags:
+                if tags["Key"] == 'Name':
+                    ins_name = tags["Value"]
+                if tags["Key"] == 'RunAfterHours':
+                    run_af_hrs = tags["Value"]
+                if tags["Key"] == 'Environment':
+                    env = tags["Value"]        
+            ins_data.writerow([ins_name,each_in.id,each_in.public_ip_address or each_in.private_ip_address,mac,env or "NotDefined",each_in.platform or 'Linux',"NotDefined",run_af_hrs or 'Yes',each_in.instance_type])
+            print(' --> Region ' + each_reg + '\'s AWS EC2 inventory file: output/' + each_prof + '-ec2_invent.csv')
+    ins_file.close()
     exit()
 
 def aws_elb():
@@ -57,12 +95,12 @@ def oci_options ():
 # Section of all MSPs
 def usage_help_arg():
     """Usage examples and syntax"""
-    print("\n Usage: python3 awsquid MSP TENANT TASK")
-    print(" Example: python3 awsquid AWS CID# EC2")
-    print(" Example: python3 awsquid AWS CID# ELB")
-    print(" Example: python3 awsquid OCI CID# HELP")
-    print(" Example: python3 awsquid GCP CID# HELP")
-    print(" Example: python3 awsquid AZURE CID# HELP\n")
+    print("\n Usage: python3 awsquid.py MSP TENANT TASK")
+    print(" Example: python3 awsquid.py AWS CID# EC2")
+    print(" Example: python3 awsquid.py AWS CID# ELB")
+    print(" Example: python3 awsquid.py OCI CID# HELP")
+    print(" Example: python3 awsquid.py GCP CID# HELP")
+    print(" Example: python3 awsquid.py AZURE CID# HELP\n")
 
 def read_args() -> Dict[str, str]:
     """Check for valid CLI arguments"""
